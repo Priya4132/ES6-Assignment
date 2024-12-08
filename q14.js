0a0cae2267d9108bfd0e2bc535ebc7fe2796f298
Priya4132 <priya.aakashmishra@gmail.com>
2024-12-08 14:36:13 +0530

    Update q14.js

--- q14.js
+++ q14.js
@@ -3,10 +3,10 @@ const profile = { name: "Charlie", age: 29, address: { city: "San Francisco", zi
 
 const updates = { age: 30, address: { zipcode: "94109", country: "USA" } };
 
-let data = {...profile,age: updates.age ?? profile.age,// Use updates.age if it exists, otherwise keep profile.age
-address: {
+let data = {...profile,...updates,address: {
     ...profile.address,            // Spread the existing address
     ...updates.address            // Overwrite or add new address properties from updates
-  }};
+  }   
+  };
 
 console.log(data);
